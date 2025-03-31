83db354d9b894eaed6bb27d635b771267f739b97
sabina-ec <cemalovicsabina@gmail.com>
2025-03-31 12:56:54 +0200

    Fixing maven version for gihub action

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class User {
+  private String user,password;
+    User(String user, String pwd){
+        this.user = user;
+        this.password = pwd;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserTest {
+    @Test
+    public void addUserTest(){
+        User user = new User("Kalle","abc123");
+    }
+
+    @Test
+    public void getNameTest(){
+        User user = new User("Kalle","abc123");
+        assertEquals("Kalle",user.getUser());
+    }
+
+    @Test
+    public void getPasswordTest(){
+        User user = new User("Kalle","abc123");
+        assertEquals("abc123",user.getPassword());
+    }
 }
+
